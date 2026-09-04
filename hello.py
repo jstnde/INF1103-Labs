471afb73d1391f3b6a21be4fe6d0cfcfa1bd3ddb
justin <nahcnitsuj@gmail.com>
2026-09-04 11:48:08 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -1,23 +1,9 @@
-print("====================================")
-print("Welcome here")
-print("My first post!")
-print("====================================")
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
-
+print("\nInstagram Profile")
+print("====================")
 print("Username: ", username)
-print("Bio: ", bio)
-print("Followers: ", followers)
-
-followers = 100
-
-followers += 50
-print("Day 1: ", followers)
-
-followers += 20
-print("Day 2: ", followers)
-
-followers -= 10
-print("Day 3: ", followers)
+print("Age: ", age)
+print("Category: ", category)
