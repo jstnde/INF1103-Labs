f8e80b97483c24384daa76aa9e5a2d1e7981a133
justin <nahcnitsuj@gmail.com>
2026-09-10 15:17:08 +0800

    handles invalid input, including negative numbers

--- auditor.py
+++ auditor.py
@@ -4,3 +4,5 @@ while True:
     userInput = input("enter a stock quantity (enter 'quit' to quit)")
     if userInput == "quit":
         break
+    elif not userInput.isdigit() or int(userInput) < 0:
+        print("invalid stock value")
