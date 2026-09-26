8f95eeed0dc8b503f7b504fd8124273608b6848c
justin <2601711@sit.singaporetech.edu.sg>
2026-09-26 14:28:40 +0800

    swapped filenames of modular and persistent auditor files cause apparently i was always in modular_auditor.py :')

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,25 +1,19 @@
 def get_valid_input():
-    product_name = input("Enter Product Name (enter 'quit' to quit): ")
+    userInput = input("Enter a stock quantity (enter 'quit' to quit): ")
 
-    if product_name.lower() == "quit":
+    if userInput.lower() == "quit":
         return None
-
-    quantity = input("Enter Quantity: ")
-
-    if not quantity.isdigit() or int(quantity) < 0:
-        print("invalid quantity value\n")
+    elif not userInput.isdigit() or int(userInput) < 0:
+        print("invalid stock value")
         return -1
     else:
-        return product_name, int(quantity)
+        return int(userInput)
 
-def process_order(current_total, inventory, last_id, valid_input):
-    last_id += 1
-    inventory.append((str(last_id), valid_input[0], str(valid_input[1])))
-    
-    print("\nNew Order Added: ")
-    print(inventory[-1][0] + ", " + inventory[-1][1] + ", " + inventory[-1][2] + "\n")
+def process_delivery(current_total, new_value):
+    return current_total + new_value
 
-    return last_id, current_total + valid_input[1]
+def calculate_tax(amount):
+    return amount * .1
 
 def generate_report(total_units, failed_attempts):
     print("Total units processed: ", total_units)
@@ -27,42 +21,20 @@ def generate_report(total_units, failed_attempts):
 
     return
 
-def load_inventory():
-    try:
-        with open("inventory.txt", "r") as file:
-            inventory = file.read().split("\n")
-            inventory.pop()
-    except FileNotFoundError:
-        return list()
-    return inventory
-
-def save_inventory(inventory):
-    with open("inventory.txt", "a") as file:
-        for item in inventory:
-            file.write(item[0] + "," + item[1] + "," + item[2] + "\n")
-    return
-
-inventory = []
 total_units = 0
 failed_attempts = 0
-last_id = 0
-
-print("Current Orders:\n")
-for order in load_inventory():
-    item = order.split(",")
-    print(item[0] + ", " + item[1] + ", " + item[2])
-    total_units += int(item[-1])
-    last_id = int(item[0])
-print()
 
 while True:
     valid_input = get_valid_input()
     if not valid_input:
         break
-    elif valid_input == -1:
+    elif valid_input < 0:
         failed_attempts += 1
     else:
-        last_id, total_units = process_order(total_units, inventory, last_id, valid_input)
+        total_units = process_delivery(total_units, valid_input)
+        print("current calculated tax: ", calculate_tax(total_units))
+        if total_units > 500:
+            print("Alert! Total Inventory Exceeding 500!")
+            break
 
-save_inventory(inventory)
 generate_report(total_units, failed_attempts)
--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,19 +1,25 @@
 def get_valid_input():
-    userInput = input("Enter a stock quantity (enter 'quit' to quit): ")
+    product_name = input("Enter Product Name (enter 'quit' to quit): ")
 
-    if userInput.lower() == "quit":
+    if product_name.lower() == "quit":
         return None
-    elif not userInput.isdigit() or int(userInput) < 0:
-        print("invalid stock value")
+
+    quantity = input("Enter Quantity: ")
+
+    if not quantity.isdigit() or int(quantity) < 0:
+        print("invalid quantity value\n")
         return -1
     else:
-        return int(userInput)
+        return product_name, int(quantity)
 
-def process_delivery(current_total, new_value):
-    return current_total + new_value
+def process_order(current_total, inventory, last_id, valid_input):
+    last_id += 1
+    inventory.append((str(last_id), valid_input[0], str(valid_input[1])))
+    
+    print("\nNew Order Added: ")
+    print(inventory[-1][0] + ", " + inventory[-1][1] + ", " + inventory[-1][2] + "\n")
 
-def calculate_tax(amount):
-    return amount * .1
+    return last_id, current_total + valid_input[1]
 
 def generate_report(total_units, failed_attempts):
     print("Total units processed: ", total_units)
@@ -21,20 +27,42 @@ def generate_report(total_units, failed_attempts):
 
     return
 
+def load_inventory():
+    try:
+        with open("inventory.txt", "r") as file:
+            inventory = file.read().split("\n")
+            inventory.pop()
+    except FileNotFoundError:
+        return list()
+    return inventory
+
+def save_inventory(inventory):
+    with open("inventory.txt", "a") as file:
+        for item in inventory:
+            file.write(item[0] + "," + item[1] + "," + item[2] + "\n")
+    return
+
+inventory = []
 total_units = 0
 failed_attempts = 0
+last_id = 0
+
+print("Current Orders:\n")
+for order in load_inventory():
+    item = order.split(",")
+    print(item[0] + ", " + item[1] + ", " + item[2])
+    total_units += int(item[-1])
+    last_id = int(item[0])
+print()
 
 while True:
     valid_input = get_valid_input()
     if not valid_input:
         break
-    elif valid_input < 0:
+    elif valid_input == -1:
         failed_attempts += 1
     else:
-        total_units = process_delivery(total_units, valid_input)
-        print("current calculated tax: ", calculate_tax(total_units))
-        if total_units > 500:
-            print("Alert! Total Inventory Exceeding 500!")
-            break
+        last_id, total_units = process_order(total_units, inventory, last_id, valid_input)
 
+save_inventory(inventory)
 generate_report(total_units, failed_attempts)
